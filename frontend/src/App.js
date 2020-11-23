b387c7758efd060d0f50907457e3bbdbe276b0f7
Sharleen Irungu <sharlirungu@gmail.com>
2020-11-23 14:26:55 +0300

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import data from './data';
+
+function App() {
+  return (
+    <div className="grid-container">
+            <header className="row">
+            <div>
+               <a className="brand" href="/">
+                  <img src="/images/logo.png" alt="Koparoo" width="100" height="75"/>
+               </a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/login">Log In</a>
+              <a href="/register">Register</a>
+            </div>            
+            </header>
+            <main>
+                <div className="row center">
+                    {
+                        data.products.map(product =>(
+                            <div key={product._id} className="card">
+                        <a href={`/product/${product._id}`}>
+                            <img className="medium" src={product.image} alt={product.name}/>
+                        </a>
+                        <div className="card-body">
+                        <a href={`/product/${product._id}`}>
+                                <h2>{product.name}</h2>
+                            </a>
+                            <div className="rating">
+                                <span> <i className="fa fa-star"></i> </span>
+                                <span> <i className="fa fa-star"></i> </span>
+                                <span> <i className="fa fa-star"></i> </span>
+                                <span> <i className="fa fa-star"></i> </span>
+                                <span> <i className="fa fa-star"></i> </span>
+                            </div>
+                        <div className="price">Ksh{product.price}</div>
+                        </div>
+                    </div>
+                        ))
+                    }
+                </div>
+            </main>
+            <footer className="row right">Koparoo 2020</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'Camera',
+            category: 'Cameras',
+            image: '/images/camera.jpg',
+            price: 120,
+            brand: 'Nikon',
+            rating: 4.5,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'DJ Deck',
+            category: 'DJ Equipment',
+            image: '/images/deck.jpg',
+            price: 121,
+            brand: 'Sony',
+            rating: 4.7,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'Electronics',
+            category: 'Electronics',
+            image: '/images/electronics.jpg',
+            price: 122,
+            brand: 'LG',
+            rating: 4,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'Music',
+            category: 'Music Equipment',
+            image: '/images/music.jpg',
+            price: 123,
+            brand: 'Nikon',
+            rating: 4.3,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'Screens',
+            category: 'Screens',
+            image: '/images/screens.jpg',
+            price: 124,
+            brand: 'Nikon',
+            rating: 4.5,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'Camera',
+            category: 'Cameras',
+            image: '/images/deck.jpg',
+            price: 125,
+            brand: 'Nikon',
+            rating: 4.9,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+    ],
+};
+export default data;
