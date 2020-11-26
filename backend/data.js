e32dfa4a743c86fb61063d546823b909b59d12c8
Sharleen Irungu <sharlirungu@gmail.com>
2020-11-26 16:10:56 +0300

    sever

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'Camera',
+            category: 'Cameras',
+            image: '/images/camera2.jpg',
+            price: 120,
+            countinStock:6,
+            brand: 'Nikon',
+            rating: 4.5,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'DJ Deck',
+            category: 'DJ Equipment',
+            image: '/images/deck.jpg',
+            price: 121,
+            countinStock:0,
+            brand: 'Sony',
+            rating: 4.7,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'Electronics',
+            category: 'Electronics',
+            image: '/images/electronics.jpg',
+            price: 122,
+            countinStock:1,
+            brand: 'LG',
+            rating: 4,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'Music',
+            category: 'Music Equipment',
+            image: '/images/music.jpg',
+            price: 123,
+            countinStock:2,
+            brand: 'Nikon',
+            rating: 4.3,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'Screens',
+            category: 'Screens',
+            image: '/images/screens.jpg',
+            price: 124,
+            countinStock:5,
+            brand: 'Nikon',
+            rating: 4.5,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'Camera',
+            category: 'Cameras',
+            image: '/images/deck.jpg',
+            price: 125,
+            countinStock:10,
+            brand: 'Nikon',
+            rating: 4.9,
+            numReviews: 20,
+            Description: 'high quality product'
+        },
+    ],
+};
+export default data;
